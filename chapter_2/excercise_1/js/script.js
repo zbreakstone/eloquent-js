4b4b77242e0240e49489cda7e15c7db64d9a66e4
Zack Breakstone <z.breakstone@gmail.com>
2016-08-29 16:30:24 -0400

    Solve Chapter 2, Example 1 using while loop in a closure

--- chapter_2/excercise_1/js/script.js
+++ chapter_2/excercise_1/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function symbolLogger(symbol) {
+  var word = '';
+  return function() {
+    while (word.length < 7) {
+      word = word + symbol;
+      console.log(word);
+    }
+  }
+}
+
+var logHash = symbolLogger('#');
+var logStar = symbolLogger('*');
+var logAt = symbolLogger('@');
+
+logHash();
+logStar();
+logAt();
